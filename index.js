10db2b593efae0ea363acac8678dae1fefd5942e
Sergey Piterman <sergeypiterman@gmail.com>
2018-02-13 22:06:24 -0800

    (fix) tweaks environment variables and package.json

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use(express.static(__dirname + "/client"));
 //   res.send("Hello World");
 // });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port);
 console.log("Listening on port ", port);
